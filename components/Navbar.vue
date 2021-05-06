ba48332bc9a7b705b5914dde898df07688eeaba9
Swadesh Behera <itswadesh@gmail.com>
2021-05-06 14:42:52 +0530

    home page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,35 +1,114 @@
 <template>
-   <nav class=" flex items-center justify-between py-10 font-bold">
-      <div>
-          <h1 class="text-xl -mt-6 ml-4"><span class="text-5xl item-center" style="color:orange">F</span>ashion Store</h1>
+  <nav class="flex flex-col pt-6 font-bold lg:py-10">
+    <div class="flex items-center justify-between w-full">
+      <div class="w-1/2 my-auto md:w-1/4">
+        <h1 class="ml-4 -mt-6 text-xl cursor-pointer">
+          <span class="text-5xl item-center" style="color: orange">F</span
+          >ashion Store
+        </h1>
       </div>
-      <div class="flex items-center">
-          <div class="">
-              <a class="ml-11 text-sm hover:text-yellow-600" href="#">New</a>
-              <a class="ml-11 text-sm hover:text-yellow-600" href="#">Catalogue</a>
-              <a class="ml-11 text-sm hover:text-yellow-600" href="#">Payment</a>
-              <a class="ml-11 text-sm hover:text-yellow-600" href="#">Shipping</a>
-              <a class="ml-11 text-sm hover:text-yellow-600" href="#">Blog</a>
-              <a class="ml-11 text-sm hover:text-yellow-600" href="#">Wishlist</a>
-              <a class="ml-11 text-sm hover:text-yellow-600" href="#">Contact</a>
-          </div>
-          <div class="flex items-center ml-72 mr-4">
-           <a class ="pl-12" href="#"><img src="/img/user.svg" alt=""></a>
-           <a class ="pl-12" href="#"><img  src="/img/search.svg" alt=""></a>
-           <a class ="pl-12" href="#"><img  src="/img/shopping-bag.svg" alt=""></a>
-          </div>
+      <div class="items-center hidden w-1/2 lg:flex">
+        <div class="flex flex-row justify-between w-full">
+          <span class="text-sm cursor-pointer hover:text-yellow-600" href="#"
+            >New</span
+          >
+          <span class="text-sm cursor-pointer hover:text-yellow-600" href="#"
+            >Catalogue</span
+          >
+          <span class="text-sm cursor-pointer hover:text-yellow-600" href="#"
+            >Payment</span
+          >
+          <span class="text-sm cursor-pointer hover:text-yellow-600" href="#"
+            >Shipping</span
+          >
+          <span class="text-sm cursor-pointer hover:text-yellow-600" href="#"
+            >Blog</span
+          >
+          <span class="text-sm cursor-pointer hover:text-yellow-600" href="#"
+            >Wishlist</span
+          >
+          <span class="text-sm cursor-pointer hover:text-yellow-600" href="#"
+            >Contact</span
+          >
+        </div>
       </div>
+      <div class="w-1/2 md:w-1/4">
+        <div class="flex items-center justify-end mr-4">
+          <span class="cursor-pointer" href="#"
+            ><img src="/img/user.svg" alt=""
+          /></span>
+          <span class="mx-12 cursor-pointer" href="#"
+            ><img src="/img/search.svg" alt=""
+          /></span>
+          <span class="cursor-pointer" href="#"
+            ><img src="/img/shopping-bag.svg" alt=""
+          /></span>
+        </div>
+      </div>
+    </div>
+    <div class="flex items-center w-full px-3 pb-2 mt-2 sm:px-10 lg:hidden">
+      <div class="flex flex-row justify-between w-full p-2 bg-gray-100">
+        <span class="text-sm cursor-pointer hover:text-yellow-600" href="#"
+          >New</span
+        >
+        <span class="text-sm cursor-pointer hover:text-yellow-600" href="#"
+          >Catalogue</span
+        >
+        <span class="text-sm cursor-pointer hover:text-yellow-600" href="#"
+          >Payment</span
+        >
+        <span
+          class="text-sm cursor-pointer sm:hidden hover:text-yellow-600"
+          href="#"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-4 h-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 13l-7 7-7-7m14-8l-7 7-7-7"
+            />
+          </svg>
+        </span>
+        <span
+          class="hidden text-sm cursor-pointer sm:flex hover:text-yellow-600"
+          href="#"
+          >Shipping</span
+        >
+        <span
+          class="hidden text-sm cursor-pointer sm:flex hover:text-yellow-600"
+          href="#"
+          >Blog</span
+        >
+        <span
+          class="hidden text-sm cursor-pointer sm:flex hover:text-yellow-600"
+          href="#"
+          >Wishlist</span
+        >
+        <span
+          class="hidden text-sm cursor-pointer sm:flex hover:text-yellow-600"
+          href="#"
+          >Contact</span
+        >
+      </div>
+    </div>
   </nav>
-  
 </template>
 
 <script>
-
 export default {
-
+  data() {
+    return {
+      serach: false,
+    }
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- components/Section.vue
+++ components/Section.vue
@@ -1,164 +1,254 @@
 <template>
-  <section class="px-10 py-12 flex mt-10">
-
-      <div class="w-2/6 ml-10">
-        <div class="">
-            <h6 class="text-lg pl-1">Offer</h6>
-            <h2 class="text-4xl font-bold leading-leading-leading-tight">OFF</h2>
-            <h2 class="text-4xl font-bold leading-leading-leading-tight ">upto <span class="text-yellow-600">&nbsp 20%</span> </h2>
-            <hr class="w-40 mt-3 line_1">
-            <h6 class="mt-3 text-sm">For all cosmetics</h6>
-        </div>
-
-        <div class="">
-
-          <ul class="my-8">
-
-             <li class="mt-4"><a class="new_trigger text-red-500 font-semibold hover:text-red-700" href="#">New</a>
-            <ul class="sub_menu ml-5">
-              <li class="py-1 pt-5"><a class="hover:text-yellow-600" href="#">Cream</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Oil</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Mask</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Lipstick</a></li>
-              <li class="pt-1"><a class="hover:text-yellow-600" href="#">Spray</a></li>
-            </ul>
-            </li>
-        
-            <li class="mt-4"><a class="new_trigger font-semibold hover:text-red-700 " href="#">For Body</a>
-            <ul class="sub_menu ml-5">
-              <li class="py-1 pt-5"><a class="hover:text-yellow-600" href="#">Cream</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Oil</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Mask</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Lipstick</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Spray</a></li>
-            </ul>
-            </li>
-
-            <li class="mt-4"><a class="new_trigger font-semibold hover:text-red-700" href="#">For Hair</a>
-            <ul class="sub_menu ml-5">
-              <li class="py-1 pt-5"><a class="hover:text-yellow-600" href="#">Cream</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Oil</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Mask</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Lipstick</a></li>
-              <li class="pt-1"><a class="hover:text-yellow-600" href="#">Spray</a></li>
-            </ul>
-            </li>
-
-            <li class="mt-4"><a class="new_trigger font-semibold hover:text-red-700" href="#">For Face</a>
-            <ul class="sub_menu ml-5">
-              <li class="py-1 pt-5"><a class="hover:text-yellow-600" href="#">Cream</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Oil</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Mask</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Lipstick</a></li>
-              <li class="pt-1"><a class="hover:text-yellow-600" href="#">Spray</a></li>
-            </ul>
-            </li>
-
-            <li class="mt-4"><a class="new_trigger font-semibold hover:text-red-700" href="#">Makeup</a>
-            <ul class="sub_menu ml-5">
-              <li class="py-1 pt-5"><a class="hover:text-yellow-600" href="#">Cream</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Oil</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Mask</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Lipstick</a></li>
-              <li class="pt-1"><a class="hover:text-yellow-600" href="#">Spray</a></li>
-            </ul>
-            </li>
-
-            <li class="mt-4"><a class="new_trigger font-semibold hover:text-red-700" href="#">Perfumes</a>
-            <ul class="sub_menu ml-5">
-              <li class="py-1 pt-5"><a class="hover:text-yellow-600" href="#">Cream</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Oil</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Mask</a></li>
-              <li class="py-1"><a class="hover:text-yellow-600" href="#">Lipstick</a></li>
-              <li class="pt-1"><a class="hover:text-yellow-600" href="#">Spray</a></li>
-            </ul>
-            </li>
-           
-          </ul>
-
-        </div>
-        
+  <section class="flex flex-col px-10 py-12 mt-10 lg:flex-row">
+    <div class="flex flex-row w-full ml-10 lg:flex-col lg:w-2/6">
+      <div class="">
+        <h6 class="pl-1 text-lg">Offer</h6>
+        <h2 class="text-4xl font-bold leading-leading-leading-tight">OFF</h2>
+        <h2 class="text-4xl font-bold leading-leading-leading-tight">
+          upto <span class="text-yellow-600">&nbsp 20%</span>
+        </h2>
+        <hr class="w-40 mt-3 line_1" />
+        <h6 class="mt-3 text-sm">For all cosmetics</h6>
       </div>
 
-      <div class="w-4/6 flex mt-16">
-        <div class="w-1/2 mr-6">
-          <div class="bg-green-200 mb-6 box-1 w-2/3 ml-auto">
-          <div class="absolute px-16 py-4 bg-white bottom-0 right-0 font-bold"><a class="hover:text-yellow-700" href="#">For Body</a></div>
-          <img src="img/cosmetics-two-bottles.png" alt=""></div>
+      <div class="">
+        <ul class="my-8">
+          <li class="mt-4">
+            <a
+              class="font-semibold text-red-500 new_trigger hover:text-red-700"
+              href="#"
+              >New</a
+            >
+            <ul class="flex flex-row ml-5 bg-yellow-400 sub_menu">
+              <li class="py-1 pt-5">
+                <a class="hover:text-yellow-600" href="#">Cream</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Oil</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Mask</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Lipstick</a>
+              </li>
+              <li class="pt-1">
+                <a class="hover:text-yellow-600" href="#">Spray</a>
+              </li>
+            </ul>
+          </li>
 
-          <div class="bg-gray-200 mb-6 box-2 w-full pt-32 relative">
-          <div class="absolute px-20 py-4 bg-white top-0 left-0 font-bold"><a class="hover:text-yellow-700" href="#">For Face</a></div>
-          <img src="img/cosmetics-yellow-jar.png" alt=""></div>
-          <div class="bg-pink-200 mb-6 box-3 w-2/3 ml-auto h-64">
-          <div class="absolute px-16 py-4 bg-white top-0 right-0 font-bold"><a class="hover:text-yellow-700" href="#">For Hair</a></div>
-          <img src="img/cosmetic-cream.png" alt=""></div>
+          <li class="mt-4">
+            <a class="font-semibold new_trigger hover:text-red-700" href="#"
+              >For Body</a
+            >
+            <ul class="ml-5 sub_menu">
+              <li class="py-1 pt-5">
+                <a class="hover:text-yellow-600" href="#">Cream</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Oil</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Mask</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Lipstick</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Spray</a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="mt-4">
+            <a class="font-semibold new_trigger hover:text-red-700" href="#"
+              >For Hair</a
+            >
+            <ul class="ml-5 sub_menu">
+              <li class="py-1 pt-5">
+                <a class="hover:text-yellow-600" href="#">Cream</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Oil</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Mask</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Lipstick</a>
+              </li>
+              <li class="pt-1">
+                <a class="hover:text-yellow-600" href="#">Spray</a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="mt-4">
+            <a class="font-semibold new_trigger hover:text-red-700" href="#"
+              >For Face</a
+            >
+            <ul class="ml-5 sub_menu">
+              <li class="py-1 pt-5">
+                <a class="hover:text-yellow-600" href="#">Cream</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Oil</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Mask</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Lipstick</a>
+              </li>
+              <li class="pt-1">
+                <a class="hover:text-yellow-600" href="#">Spray</a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="mt-4">
+            <a class="font-semibold new_trigger hover:text-red-700" href="#"
+              >Makeup</a
+            >
+            <ul class="ml-5 sub_menu">
+              <li class="py-1 pt-5">
+                <a class="hover:text-yellow-600" href="#">Cream</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Oil</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Mask</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Lipstick</a>
+              </li>
+              <li class="pt-1">
+                <a class="hover:text-yellow-600" href="#">Spray</a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="mt-4">
+            <a class="font-semibold new_trigger hover:text-red-700" href="#"
+              >Perfumes</a
+            >
+            <ul class="ml-5 sub_menu">
+              <li class="py-1 pt-5">
+                <a class="hover:text-yellow-600" href="#">Cream</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Oil</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Mask</a>
+              </li>
+              <li class="py-1">
+                <a class="hover:text-yellow-600" href="#">Lipstick</a>
+              </li>
+              <li class="pt-1">
+                <a class="hover:text-yellow-600" href="#">Spray</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="flex w-full mt-16 lg:w-4/6">
+      <div class="w-1/2 mr-6">
+        <div class="w-2/3 mb-6 ml-auto bg-green-200 box-1">
+          <div class="absolute bottom-0 right-0 px-16 py-4 font-bold bg-white">
+            <a class="hover:text-yellow-700" href="#">For Body</a>
+          </div>
+          <img src="img/cosmetics-two-bottles.png" alt="" />
         </div>
 
-        <div class="w-1/2 mr-6">
-          <div class="bg-purple-300 mb-6 box-4 w-2/3">
-          <div class="absolute px-16 py-4 bg-white bottom-0 left-0 font-bold"><a class="hover:text-yellow-700" href="#">Perfumes</a></div>
-          <img src="img/cosmetics.png" alt=""></div>
-          <div class="bg-yellow-200 mb-6 box-5 w-full">
-          <div class="absolute px-16 py-4 bg-white top-0 right-0 font-bold"><a class="hover:text-yellow-700" href="#">Make up</a></div>
-          <img src="img/cosmetics-bottles.png" alt=""></div>
-          <div class="bg-blue-200 mb-6 box-6 w-2/3 pt-32 pb-12 pr-6 mt-40 relative">
-          <div class="absolute px-16 py-4 bg-white top-0 left-0 font-bold"><a class="hover:text-yellow-700" href="#">For Men</a></div>
-          <img src="img/cosmetics-jar.png" alt=""></div>
+        <div class="relative w-full pt-32 mb-6 bg-gray-200 box-2">
+          <div class="absolute top-0 left-0 px-20 py-4 font-bold bg-white">
+            <a class="hover:text-yellow-700" href="#">For Face</a>
+          </div>
+          <img src="img/cosmetics-yellow-jar.png" alt="" />
+        </div>
+        <div class="w-2/3 h-64 mb-6 ml-auto bg-pink-200 box-3">
+          <div class="absolute top-0 right-0 px-16 py-4 font-bold bg-white">
+            <a class="hover:text-yellow-700" href="#">For Hair</a>
+          </div>
+          <img src="img/cosmetic-cream.png" alt="" />
         </div>
       </div>
 
+      <div class="w-1/2 mr-6">
+        <div class="w-2/3 mb-6 bg-purple-300 box-4">
+          <div class="absolute bottom-0 left-0 px-16 py-4 font-bold bg-white">
+            <a class="hover:text-yellow-700" href="#">Perfumes</a>
+          </div>
+          <img src="img/cosmetics.png" alt="" />
+        </div>
+        <div class="w-full mb-6 bg-yellow-200 box-5">
+          <div class="absolute top-0 right-0 px-16 py-4 font-bold bg-white">
+            <a class="hover:text-yellow-700" href="#">Make up</a>
+          </div>
+          <img src="img/cosmetics-bottles.png" alt="" />
+        </div>
+        <div
+          class="relative w-2/3 pt-32 pb-12 pr-6 mt-40 mb-6 bg-blue-200 box-6"
+        >
+          <div class="absolute top-0 left-0 px-16 py-4 font-bold bg-white">
+            <a class="hover:text-yellow-700" href="#">For Men</a>
+          </div>
+          <img src="img/cosmetics-jar.png" alt="" />
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style scopped>
-.line_1{
-    border-top: 4px solid rgba(255, 68, 0, 0.767);
+.line_1 {
+  border-top: 4px solid rgba(255, 68, 0, 0.767);
 }
-.sub_menu{
+.sub_menu {
   max-height: 0;
   overflow: hidden;
 }
-.box-1{
+.box-1 {
   position: relative;
   height: 24rem;
 }
-.box-1 img{
+.box-1 img {
   position: absolute;
   top: -5rem;
 }
-.box-3{
+.box-3 {
   position: relative;
   height: 24rem;
 }
-.box-3 img{
+.box-3 img {
   position: absolute;
   bottom: -15rem;
 }
-.box-4{
+.box-4 {
   position: relative;
   height: 22rem;
   margin-top: 18rem;
 }
-.box-4 img{
+.box-4 img {
   position: absolute;
   top: -14rem;
   left: 15%;
 }
-.box-5{
+.box-5 {
   position: relative;
   height: 12rem;
 }
-.box-5 img{
+.box-5 img {
   position: absolute;
   bottom: -8rem;
 }
-
-
-
 </style>
